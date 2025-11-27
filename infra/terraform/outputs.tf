b872ee0e0cb6ed6c669c9988159dddea1d427851
Said Kasimov <kasimovsfdy@outlook.com>
2025-11-26 23:31:59 -0600

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -5,3 +5,11 @@ output "ecs_cluster_name" {
 output "rds_endpoint" {
   value = aws_db_instance.orders_db.address
 }
+
+data "aws_secretsmanager_secret" "rds_password_secret" {
+  name = "rds-master-password-for-project-x"
+}
+
+data "aws_secretsmanager_secret_version" "rds_password_version" {
+  secret_id = data.aws_secretsmanager_secret.rds_password_secret.id
+}
