b21c3905690f3e7048cb288538e77194bdea68f7
Said Kasimov <kasimovsfdy@outlook.com>
2025-11-26 23:33:07 -0600

    Update rds.tf

--- infra/terraform/rds.tf
+++ infra/terraform/rds.tf
@@ -33,11 +33,12 @@ resource "aws_security_group" "rds" {
 
 resource "aws_db_instance" "orders_db" {
   identifier              = "${var.project_name}-db"
-  engine                  = "postgres" 
+  engine                  = "postgres"
+  engine_version          = "17.6"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   username                = var.rds_username
-  password                = var.rds_password
+  password                = "Said-kalfa-pass2$"
   db_subnet_group_name    = aws_db_subnet_group.orders.name
   vpc_security_group_ids  = [aws_security_group.rds.id]
   publicly_accessible     = false
